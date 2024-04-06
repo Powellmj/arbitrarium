54fce582d6bfff8124d9afa0b5765a54058cce9f
Powellmj <michaelpowell@Michaels-MacBook-Pro.local>
2024-04-06 16:02:06 -0400

    fix placeholdeer

--- frontend/src/components/TablePage.js
+++ frontend/src/components/TablePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { Table, Container, FormControl, Button, Navbar, InputGroup } from 'react-bootstrap';
 import EditItemModal from "./EditItemModel"
@@ -43,6 +43,7 @@ function HomePage() {
         const options = ["Hello, sunshine!", "Howdy, partner!", "Hey, hi, hello!", "What’s kickin’, little chicken?", "Peek-a-boo!", "Howdy-doody!", "Hey there, freshman!", "My name's Ralph, and I'm a bad guy.", "Welcome!", "I come in peace!", "Put that cookie down!", "Ahoy, matey!", "Hiya!", "'Ello, gov'nor!", "Top of the mornin’ to ya!", "What’s crackin’?", "GOOOOOD MORNING, VIETNAM!", "Howdy, howdy, howdy!", "Hello, my name is Inigo Montoya.", "I'm Batman.", "So, at last, we meet for the first time, for the last time!", "Here's Johnny!", "Ghostbusters, whatya want?", "Yo!", "Whaddup.", "Greetings and salutations!", "Doctor.", "‘Ello, mate.", "Oh, yoooouhoooo!", "How you doin'?", "I like your face.", "What's cookin', good lookin'?", "Why, hello there!", "Hey, boo.", "Listen!", "Generic Greeting!"]
         return options[Math.floor(Math.random() * options.length)]
       }
+      const placeholder = useMemo(() => setPlaceHolder(), []);
 
     const handlePushConfig = () => {
         dispatch(pushConfig())
@@ -187,7 +188,7 @@ function HomePage() {
             <Navbar style={{ position: 'fixed', width: '100vw' }} bg="light" expand="lg">
                 <InputGroup>
                     <Button className="add/edit" style={{ marginRight: "10px" }} onClick={e => { handleEditClick(defaultEntry, e) }} variant="primary" size="sm">Add Entry</Button>
-                    <FormControl type="text" placeholder={setPlaceHolder()} value={filter} onChange={e => { setFilter(e.target.value) }} />
+                    <FormControl type="text" placeholder={placeholder} value={filter} onChange={e => { setFilter(e.target.value) }} />
                     <InputGroup.Append>
                         <Button onClick={() => { handlePushConfig() }} variant="info">Push Config</Button>
                     </InputGroup.Append>
